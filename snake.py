aeea6a262f5b4b0322bd23764f71acdfde381ac3
formazione <gatto.gio@gmail.com>
2020-07-22 10:21:41 +0200

    new snake.py version

--- snake.py
+++ snake.py
@@ -19,13 +19,18 @@ class Snake():
 
 
     def start(self):
-        self.head = [int(BOARD_SIZE / 4), int(BOARD_SIZE / 4)]
+        # coordinates of the head 5, 5
+        self.head = [
+            int(BOARD_SIZE / 4), # x = self.head[0] = 5 = 20 / 4
+            int(BOARD_SIZE / 4)] # y ? self.head[1] = 5 = 20 / 4
+        # self.head[0] = x = 5
+        # self.head[1] = x = 5
         self.body = [[self.head[0], self.head[1]],
                      [self.head[0] - 1, self.head[1]],
                      [self.head[0] - 2, self.head[1]]
                      ]
+        #   [ ][ ][ ] => right
         self.direction = "RIGHT"
-
 
     def change_direction_to(self, dir):
         if dir == "RIGHT" and not self.direction == "LEFT":
@@ -38,7 +43,8 @@ class Snake():
             self.direction = "DOWN"
 
     def move(self, food_pos):
-        ''' Move the snake to the desired direction by adding the head to that direction
+        ''' Move the snake to the desired
+        direction by adding the head to that direction
             and remove the tail if the snake does not eat food
         '''
         if self.direction == "RIGHT":
@@ -49,23 +55,29 @@ class Snake():
             self.head[1] -= 1
         if self.direction == "DOWN":
             self.head[1] += 1
-
+        # [][][]
         self.body.insert(0, list(self.head))
+        # [][][][]
         if self.head == food_pos:
+            # [][][][]  it grows after he ate
             return 1
         else:
             "If do not eat... same size"
             self.body.pop()
+            # pop([])   [][][]  it stays of the same size, but moves
             return 0
 
     def check_collision(self):
-        # Check if the head collides with the edges of the board
-        # if 1 => GAME OVER
+        # Checks collision with border or himself
 
         conditions = (
-        self.head[0] >= 20 or self.head[0] < 0,
-        self.head[1] > 20 or self.head[1] < 0,
-        [x for x in self.body[1:] if self.head == x]
+            # x axis limits  <0 ..... >20
+            self.head[0] >= 20 or self.head[0] < 0,
+            # y axis
+            self.head[1] > 19 or self.head[1] < 0,
+            # checks if you hit yourself
+            # comprehension list - a for loop with a python syntax
+            [x for x in self.body[1:] if self.head == x]
         )
         if any(conditions):
             return 1
@@ -75,23 +87,20 @@ class Snake():
 
 class FoodSpawner():
     def __init__(self):
-        self.head = [random.randrange(1, BOARD_SIZE), random.randrange(1, BOARD_SIZE)]
+        self.food_pos = [random.randrange(1, BOARD_SIZE), random.randrange(1, BOARD_SIZE)]
         self.is_food_on_screen = True
 
     def spawn_food(self):
         if self.is_food_on_screen == False:
-            self.head = [random.randrange(1, BOARD_SIZE), random.randrange(1, BOARD_SIZE)]
+            self.food_pos = [random.randrange(1, BOARD_SIZE), random.randrange(1, BOARD_SIZE)]
             self.is_food_on_screen = True
-        return self.head
+        return self.food_pos
 
     def set_food_on_screen(self, bool_value):
         self.is_food_on_screen = bool_value
 
 
 #                         2 main objects
-snake = Snake()
-food_spawner = FoodSpawner()
-
 
 
 def draw_head(pos):
@@ -104,16 +113,10 @@ def draw_head(pos):
             BLOCK_SIZE,
             BLOCK_SIZE))
 
-def blit_head(pos):
-    "Use a surface, instead"
-    window.blit(head, (pos[0] * BLOCK_SIZE, pos[1] * BLOCK_SIZE,))
-
-def blit_blacktail(pos):
-    "Use a surface, instead"
-    window.blit(head, (pos[0] * BLOCK_SIZE, pos[1] * BLOCK_SIZE,))
 
 def draw_body(pos):
     pygame.draw.rect(window, (0, 128, 0), pygame.Rect(pos[0] * BLOCK_SIZE, pos[1] * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
+
 
 def delete_tail(pos):
     pygame.draw.rect(window, (0, 0, 0), pygame.Rect(snake.body[-1][0] * BLOCK_SIZE, snake.body[-1][1] * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
@@ -126,19 +129,20 @@ def delete_fruit(pos, food_pos):
     gfxdraw.filled_circle(window, x, y, r, (0, 0, 0))
 
 
-def draw_fruit(pos, food_pos):
+def draw_fruit(food_pos):
     gfxdraw.filled_circle(window, food_pos[0] * BLOCK_SIZE + 10, food_pos[1] * BLOCK_SIZE + 10, 9, (255, 0, 0))
 
 
-pygame.init()
+# To write on the window surface on the screen
 
-
-def write(text_to_show, x=0, y=0, middle=0):
-    font = pygame.font.SysFont(text_to_show, 24)
+def write(text_to_show, x=0, y=0, middle="both"):
+    "It write in the middle by default, if not middle='both' middle='x'"
     text = font.render(text_to_show, 1, pygame.Color("Coral"))
-    w = h = BOARD_SIZE * BLOCK_SIZE
-    if middle:
-        text_rect = text.get_rect(center=((w // 2, h // 2)))
+    if middle == "x":
+        text_rect = text.get_rect(center=((size // 2, y)))
+        window.blit(text, text_rect)      
+    elif middle == "both":
+        text_rect = text.get_rect(center=((size // 2, size // 2)))
         window.blit(text, text_rect)
     else:
         window.blit(text, (x, y))
@@ -148,7 +152,6 @@ def write(text_to_show, x=0, y=0, middle=0):
 def restart():
     global GAME_SPEED
 
-
     GAME_SPEED = 8
     window.fill((0, 0, 0))
     snake.start()
@@ -156,8 +159,17 @@ def restart():
 
 
 def press_to_start():
-    global loop
-    write("Press any s to start", middle=1)
+    "Initial menu"
+    global loop, snake, food_spawner
+    global font, size
+
+    pygame.init()
+    font = pygame.font.SysFont("Arial", 24)
+    size = BOARD_SIZE * BLOCK_SIZE # 400 20x20
+    snake = Snake()
+    food_spawner = FoodSpawner()
+    write("Python vs Snake", y=30, middle="x")
+    write("Press s to start")
     while True:
         event = pygame.event.wait()
         if event.type == pygame.QUIT:
@@ -175,6 +187,7 @@ def press_to_start():
 
 
 def start():
+    "Starts the game"
     global GAME_SPEED, score, loop
 
     food_pos = food_spawner.spawn_food()
@@ -195,15 +208,12 @@ def start():
                 elif event.key == pygame.K_LEFT:
                     snake.change_direction_to("LEFT")
         if snake.move(food_pos) == 1:
-            delete_fruit(pos, food_pos)
+            # delete_fruit(pos, food_pos)
             score += 1
             food_spawner.set_food_on_screen(False)
             GAME_SPEED += 1
             food_pos = food_spawner.spawn_food()
 
-
-        # window.fill(pygame.Color(225, 225, 225))
-        # Draw snake
         head = 1
         for pos in snake.body:
             if head == 1:
@@ -212,7 +222,7 @@ def start():
             else:
                 draw_body(pos)
         delete_tail(pos)
-        draw_fruit(pos,food_pos)
+        draw_fruit(food_pos)
 
         if snake.check_collision() == 1:
             loop = 0
